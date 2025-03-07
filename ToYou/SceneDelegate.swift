6fe9554a1728c8daabe1e4d33038652ea6867c87
Rudy-009 <rofxnaos@gmail.com>
2025-03-07 22:10:39 +0900

    docs: 애셋 정리

--- ToYou/SceneDelegate.swift
+++ ToYou/SceneDelegate.swift
@@ -12,8 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let rootNavigation = UINavigationController(rootViewController: LoginViewController())
-        rootNavigation.isNavigationBarHidden = true
+        let rootNavigation = LoginViewController()
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
