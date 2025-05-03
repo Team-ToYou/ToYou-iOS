472986aee176a02aa0a53bc1676a9749bfdfdeae
Rudy-009 <rofxnaos@gmail.com>
2025-05-04 00:09:54 +0900

    feat: 토큰 유효성 검사를 통한 자동 로그인 구현

--- ToYou/Services/authApiService.swift
+++ ToYou/Services/authApiService.swift
@@ -17,7 +17,6 @@ enum ReissueCode: String {
 class APIService {
     static func reissueRefreshToken( completion: @escaping(ReissueCode) -> Void) {
         guard let refreshToken = KeychainService.get(key: K.Key.refreshToken) else {
-            print(100)
             completion(.error);
             return
         } // 없는 토큰은 무효한 토큰과 마찬가지
@@ -48,12 +47,15 @@ class APIService {
                 case ReissueCode.success.rawValue:
                     completion(.success)
                 case ReissueCode.expired.rawValue:
+                    // error, expired 모두 login으로 이동해야함, 내부에서 구현.
+                    // why? 여러 곳에서 쓰일것이고 공통된 동작을 할 것이기 때문에 동작을 여기서 미리 지정해도 괜찮다.
+                    RootViewControllerService.toLoginViewController()
                     completion(.expired)
                 default: // 다른 모든 경우는 error 처리
+                    RootViewControllerService.toLoginViewController()
                     completion(.error)
                 }
-                
-            case .failure(let error):
+            case .failure(let error): // 이 에러는 서버에러일 수 있어서, 어떤 동작을 해야할지 생각해볼 필요가 있음
                 print("reissue error", error)
                 completion(.error)
             }
@@ -63,5 +65,47 @@ class APIService {
     struct ReissueResult: Codable {
         let refreshToken: String?
         let accessToken: String?
+    }
+    
+    static func loginWithApple(authorizationCode: String, completion: @escaping(Bool) -> Void) {
+        let tail = "/auth/apple"
+        let url = K.URLString.baseURL + tail
+        let headers: HTTPHeaders = [
+            "accept": "*/*",
+            "authorizationCode": authorizationCode
+        ]
+        // JSONEncoding 사용하기
+        AF.request(url,
+                   method: .post,
+                   headers: headers)
+        .responseDecodable(of: ToYouResponse<AppleLoginResult>.self) { response in
+            switch response.result {
+            case .success(let apiResponse):
+                if apiResponse.isSuccess {
+                    let accessToken = apiResponse.result!.accessToken
+                    let refreshToken = apiResponse.result!.refreshToken
+                    print("accessToken  \(accessToken)")
+                    print("refreshToken \(refreshToken)")
+                    let _ = KeychainService.add(key: K.Key.accessToken, value: accessToken)
+                    let _ = KeychainService.add(key: K.Key.refreshToken, value: refreshToken)
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            case .failure(let error):
+                completion(false)
+                print("\(tail) send 요청 실패: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    struct AppleLoginResult: Codable {
+        let isUser: Bool?
+        let accessToken: String
+        let refreshToken: String
+    }
+    
+    enum AUTHCode: String {
+        case invalidToken = "OAUTH401", success = "COMMON200"
     }
 }
