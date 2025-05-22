a7f63d41eed0c0b64dd03d3e4be51535f3c0fc96
Rudy-009 <rofxnaos@gmail.com>
2025-05-22 09:49:01 +0900

    feat: fcm 토큰 저장 및 업데이트 API 연동

--- ToYou/SceneDelegate.swift
+++ ToYou/SceneDelegate.swift
@@ -16,23 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-
-        // 1. 유저의 토큰을 갱신해준다.
-            // 토큰이 없는 경우 -> toLogin
-            // 토큰이 있는 경우
-                // refresh가 유효한 경우 -> 갱신 -> toBase
-                // 만료된 경우 -> toLogin
-        // 2. 유저가 회원가입 절차를 완료했는지 확인한다.
-            // 토큰이 없는 경우 -> toLogin (첫 회원 혹은 로그아웃 혹은 탈퇴)
-            // 토큰이 있는 경우
-                // 필수 내용이 누락된 경우 -> toSignUp -> toTutorial -> toBase
-                // 필수 내용이 모두 기재된 경우 -> toBase & myPage 내용 저장
+        
         guard let _ = KeychainService.get(key: K.Key.accessToken) else {
             RootViewControllerService.toLoginViewController()
             return
         }
         
         AuthAPIService.isUserFinishedSignUp { code in
+            print("access token : ", KeychainService.get(key: K.Key.accessToken)!)
+            print("refresh token : ", KeychainService.get(key: K.Key.refreshToken)!)
             switch code {
             case .finished:
                 RootViewControllerService.toBaseViewController()
