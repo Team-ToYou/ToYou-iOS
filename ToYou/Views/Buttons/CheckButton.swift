16e05650c90d214b3e06d0e1a968a92cff0e1937
Rudy-009 <rofxnaos@gmail.com>
2025-06-15 14:51:45 +0900

    feat: 중복확인 버튼 관련 QA 반영

--- ToYou/Views/Buttons/CheckButton.swift
+++ ToYou/Views/Buttons/CheckButton.swift
@@ -11,7 +11,7 @@ class CheckButton: UIButton {
     
     private lazy var mainLabel = UILabel().then {
         $0.text = "중복확인"
-        $0.font = UIFont(name: K.Font.s_core_regular , size: 12)
+        $0.font = UIFont(name: K.Font.s_core_regular , size: 13)
         $0.textColor = .black01
     }
     
