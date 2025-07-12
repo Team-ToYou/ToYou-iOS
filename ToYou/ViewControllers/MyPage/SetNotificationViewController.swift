920000e32bf0a3e955da11a8367976d6fb524809
Rudy-009 <rofxnaos@gmail.com>
2025-07-12 14:30:13 +0900

    feat: 현재 알림 허용 상태에 따라 알림 설정 뷰의 토글이 달라짐

--- ToYou/ViewControllers/MyPage/SetNotificationViewController.swift
+++ ToYou/ViewControllers/MyPage/SetNotificationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class SetNotificationViewController: UIViewController {
     
@@ -16,15 +17,87 @@ class SetNotificationViewController: UIViewController {
         self.view = setNotificationView
         self.setupButtonActions()
         self.navigationController?.isNavigationBarHidden = true
+        checkNotificationAuthorizationStatus()
     }
     
     private func setupButtonActions() {
         setNotificationView.navigationBar.popUpViewButton.addTarget(self, action: #selector(popStack), for: .touchUpInside)
+        setNotificationView.toggle.addTarget(self, action: #selector(toggleNotificationState), for: .valueChanged)
     }
     
     @objc
     private func popStack() {
         dismiss(animated: false, completion: nil)
+    }
+    
+    @objc
+    private func toggleNotificationState() {
+        switch setNotificationView.toggle.isOn {
+        case true: // false -> true로 바뀜
+            print("false -> true")
+        case false: // false로 바뀜
+            print("true -> false")
+        }
+    }
+    
+    func checkNotificationAuthorizationStatus() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                DispatchQueue.main.async {
+                    self.setNotificationView.toggle.isOn = true
+                }
+                print("알림 허용됨")
+                // 알림을 사용할 수 있습니다.
+            case .denied:
+                self.requestNotificationAuthorization()
+                DispatchQueue.main.async {
+                    self.setNotificationView.toggle.isOn = false
+                }
+                print("알림 거부됨")
+                // 사용자에게 알림 설정 화면으로 이동하도록 안내합니다.
+            case .notDetermined:
+                self.requestNotificationAuthorization()
+                print("알림 권한 미결정")
+                // 사용자에게 알림 권한을 요청합니다.
+            case .provisional:
+                DispatchQueue.main.async {
+                    self.setNotificationView.toggle.isOn = true
+                }
+                print("프로비저널 알림 허용됨")
+                // 제한적인 알림을 사용할 수 있습니다.
+            case .ephemeral:
+                DispatchQueue.main.async {
+                    self.setNotificationView.toggle.isOn = true
+                }
+                print("임시 알림 허용됨")
+                // 특정 조건에서만 알림을 사용할 수 있습니다.
+            @unknown default:
+                DispatchQueue.main.async {
+                    self.setNotificationView.toggle.isOn = false
+                }
+                print("알 수 없는 알림 상태")
+            }
+        }
+    }
+    
+    func requestNotificationAuthorization() {
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { (granted, error) in
+                if let error = error {
+                    print("알림 권한 요청 중 오류 발생: \(error.localizedDescription)")
+                }
+                if granted {
+                    print("알림 권한 허용됨")
+                    // 알림을 사용할 수 있습니다.
+                } else {
+                    print("알림 권한 거부됨")
+                    // 사용자에게 알림 설정 화면으로 이동하도록 안내합니다.
+                }
+            }
+        )
     }
 
 }
